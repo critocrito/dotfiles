e76d21005167ad334dd4d37b3cf2e55df9c22753
crito <crito@cryptodrunks.net>
2015-03-16 16:52:21 +0100

    Added new mailaccount.

--- mail/Applications/offlineimap-helpers.py
+++ mail/Applications/offlineimap-helpers.py
@@ -16,3 +16,22 @@ def localtransfolder_cryptodrunks(foldername):
         retval = re.sub("cryptodrunks\.", "", foldername)
     retval = re.sub("\.", "/", retval)
     return retval
+
+
+def oimaptransfolder_tacticaltech(foldername):
+    if(foldername == "INBOX"):
+        retval = "tacticaltech"
+    else:
+        retval = foldername
+    retval = re.sub("/", ".", retval)
+    return retval
+
+def localtransfolder_tacticaltech(foldername):
+    if(foldername == "tacticaltech"):
+        retval = "INBOX"
+    else:
+        # remove leading 'tacticaltech.'
+        #retval = re.sub("INBOX\.", "", foldername)
+        retval = foldername
+    retval = re.sub("\.", "/", retval)
+    return retval

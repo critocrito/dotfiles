6b5cc95d8ad1cd69c5c51ec050fb820ceda71224
Christo <christo@cryptodrunks.net>
2017-08-05 16:33:24 +0200

    refactor(mail): Split up offlineimap config into multiple files, and use timers to run it.

--- mail/helpers.py
+++ mail/helpers.py
@@ -1,6 +1,7 @@
 import re
 from subprocess import check_output
 
+
 def get_pass(account):
     return check_output("/usr/bin/pass mail/" + account, shell=True).splitlines()[0]
 
@@ -13,6 +14,24 @@ def oimaptransfolder_cryptodrunks(foldername):
     return retval
 
 def localtransfolder_cryptodrunks(foldername):
+    if(foldername == "cryptodrunks"):
+        retval = "INBOX"
+    else:
+        # remove leading 'cryptodrunks.'
+        retval = re.sub("cryptodrunks\.", "", foldername)
+    retval = re.sub("\.", "/", retval)
+    return retval
+
+
+def oimaptransfolder_cryptodrunks2(foldername):
+    if(foldername == "INBOX"):
+        retval = "cryptodrunks"
+    else:
+        retval = "cryptodrunks." + foldername
+    retval = re.sub("/", ".", retval)
+    return retval
+
+def localtransfolder_cryptodrunks2(foldername):
     if(foldername == "cryptodrunks"):
         retval = "INBOX"
     else:

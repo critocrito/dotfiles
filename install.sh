303315d00c2c9cbe5e276f24112b8e7964ac23ee
Christo <christo@cryptodrunks.net>
2019-07-15 08:04:57 +0200

    enable exa as alternative to ls

--- install.sh
+++ install.sh
@@ -183,7 +183,7 @@ fi
 for F in functions env aliases profile rc
 do
   for T in shell zsh ssh git gnupg xdg xorg \
-                 grep fzf emacs systemd firefox \
+                 grep exa fzf emacs systemd firefox \
                  node python haskell ruby rust
   do
     append_to_file $T $F
